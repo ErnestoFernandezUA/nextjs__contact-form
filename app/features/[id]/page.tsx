acf7dd5eaaccbee2f4d8a49f45f20ae9eee8b945
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-08-31 04:00:54 +0300

    fix noparams features

--- app/features/[id]/page.tsx
+++ app/features/[id]/page.tsx
@@ -5,9 +5,17 @@ import styles from './page.module.css'
 export default function Features() {
   const params = useParams();
 
+  if (!params) {
+    return (
+      <section className={styles.mainContent}>
+        <h1 className={styles.title}>Some problem with params</h1>
+      </section>
+    );
+  }
+
   return (
     <section className={styles.mainContent}>
       <h1 className={styles.title}>features {params.id}</h1>
     </section>
-  )
+  );
 }
